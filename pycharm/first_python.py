58123a29adfd2e68167e40bf291f2c7af1cbf37d
Lee-Sooyoung <zxzx7845@naver.com>
2020-02-09 18:57:52 +0900

    week5
    week5

--- pycharm/first_python.py
+++ pycharm/first_python.py
@@ -1,29 +1,39 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
 import requests
 from bs4 import BeautifulSoup
 
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.dbsparta
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908', headers=headers)
+## HTML을 주는 부분
+@app.route('/')
+def home():
+   return render_template('index.html')
 
-soup = BeautifulSoup(data.text, 'html.parser')
+@app.route('/order', methods=['POST'])
+def order_add():
+    name_receive = request.form['name_give']
+    count_receive = request.form['count_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
 
-musics = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+    order = {
+        'name': name_receive,
+        'count': count_receive,
+        'address': address_receive,
+        'phone': phone_receive
+    }
+    db.orders.insert_one(order)
 
-rank = 1
-for music in musics:
-    a_tag = music.select_one('td.info > a.title.ellipsis')
-    if a_tag is not None:
-        title = a_tag.text
-        singer = music.select_one('td.info > a.artist.ellipsis').text
-        number = music.select_one('td.number').text
+    return jsonify({'result':'success'})
 
-        doc = {
-            'title': title,
-            'singer': singer,
-            'number': number
-        }
-        db.musics.insert_one(doc)
-        rank += 1
+@app.route('/order', methods=['GET'])
+def order_listing():
+    result = list(db.orders.find({}, {'_id': 0}))
+    return jsonify({'result':'success','orders':result})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
