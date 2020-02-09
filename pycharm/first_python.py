c73f1184f5b1d8d57645c523e1ac5292b8be4eb0
Lee-Sooyoung <zxzx7845@naver.com>
2020-02-09 11:25:07 +0900

    hw4
    hw4

--- pycharm/first_python.py
+++ pycharm/first_python.py
@@ -1,23 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-# 타겟 URL을 읽어서 HTML를 받아오고,
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://movie.naver.com/movie/sdb/rank/rpeople.nhn?date=20200117',headers=headers)
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
-# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
-# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
-# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908', headers=headers)
+
 soup = BeautifulSoup(data.text, 'html.parser')
 
-#############################
-# (입맛에 맞게 코딩)
-#############################
-actors = soup.select('#old_content > table > tbody > tr')
-for actor in actors:
-    # movie 안에 a 가 있으면,
-    a_tag = actor.select_one('td.title > a')
-    if a_tag is not None:
-        # a의 text를 찍어본다.
-        print (a_tag.text)
+musics = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
+rank = 1
+for music in musics:
+    a_tag = music.select_one('td.info > a.title.ellipsis')
+    if a_tag is not None:
+        title = a_tag.text
+        singer = music.select_one('td.info > a.artist.ellipsis').text
+        number = music.select_one('td.number').text
+
+        doc = {
+            'title': title,
+            'singer': singer,
+            'number': number
+        }
+        db.musics.insert_one(doc)
+        rank += 1
